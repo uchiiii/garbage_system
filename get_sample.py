87e9c57eedced378ff96a62154ea7f141b6f84ed
Ryosuke Horiuchi <32426869+uchiiii@users.noreply.github.com>
2018-06-11 18:35:02 +0900

    add
    add the description at the beginning

--- get_sample.py
+++ get_sample.py
@@ -1,3 +1,8 @@
+#カメラで指定した枚数だけ撮影し、指定のファイルにいれる.
+#実行例１：猫の写真を連続で100枚とる.  ./catに出力されるファイル名はpic0.pngからpic99.png
+#	>> python get_sample.py ./cat 100 0
+#実行例２: 猫の写真を連続で50枚とる. ただし、すでに./catにはpic4.pngまで存在してる.  ./catに出力されるファイル名はpic5.pngからpic54.png
+#	>> python get_sample.py ./cat 
 import cv2
 import time 
 import sys
