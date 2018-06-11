9b855c3aefe0efd8db0103220aaac7031e7e0d04
uchiiii <uchikun17@gmail.com>
2018-06-11 12:06:31 +0900

    return 1 when something is put.

--- offset.ino
+++ offset.ino
@@ -1,6 +1,7 @@
 int analogPin = 3;
-const int triger_val = 50;
+int analogPin = 221;
 
+const int triger_val = 50;
 int val = 0;
 
 void setup(){
@@ -13,11 +14,12 @@ void loop(){
 	val = analogRead(analogPin);
 	
 	//値の表示
-	Serial.println(val);
+	//Serial.println(val);
 
 	//電圧が一定以上の時
 	if(val > triger_val){
-		
+		//int 1を送る
+		Serial.write(1)			
 		//30秒待つ
 		delay(30000);
 	}	
