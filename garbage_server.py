47653c3016f2b296075d47a77ae5b5760d70e393
uchiiii <uchikun17@gmail.com>
2018-06-22 16:49:38 +0900

    update

--- garbage_server.py
+++ garbage_server.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 '''
 On every occastion, 0 refers to pet(bottle), 1 bin, 2 can.
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import datetime
 import numpy as np
 import cv2
@@ -17,7 +16,7 @@ import time, threading
 garbage_amount = {'pet':0, 'bin':0, 'can':0}
 
 #dictionary to store the total amount of garbage
-total_amount = [{},{},{}]
+total_amount = [{"2018/4": 3, "2018/5" : 8},{"2018/4": 4, "2018/5": 6},{"2018/4": 12, "2018/5": 2}]
 
 #The class below is for the process with arduino
 class Triger():
@@ -29,9 +28,9 @@ class Triger():
         self.thread.start()
 
     def waiting(self):
-        with serial.Serial('COM3',9600,timeout=1) as ser:
+        with serial.Serial('/dev/ttyACM0',9600,timeout=1) as ser:
             while True:
-                if ser.inwaiting()>0:
+                if ser.in_waiting > 0:
                     frame = self.take_photo()
                     #image recognition
                     infer = self.image_recognition(frame)
@@ -73,20 +72,11 @@ class Triger():
 #below is the server setting.
 app = Flask(__name__)
 
-=======
-from flask import Flask, render_template
-import datetime
-
-app = Flask(__name__)
-
-
->>>>>>> 2b6664dd254c4eab8c67502ee99bc94de407a756
 #th process of the top page
 @app.route("/")
 def put_out_num():
     now = datetime.datetime.now()
     timeString = now.strftime("%Y-%m-%d %H:%M")
-<<<<<<< HEAD
     templateData = {
         'time': timeString,
         'num_pet': garbage_amount['pet'],
@@ -95,45 +85,38 @@ def put_out_num():
     }
     return render_template('main.html', **templateData)
 
+@app.route('/reset')
+def load_html():
+    return render_template('reset.html')
 
-@app.route('/reset', methods=['GET'])
+@app.route('/draw_graph')
 def reset():
     today = datetime.date.today()
     i = 0
-    year_month = str(today.year) + ' ' + str(today.month) 
+    year_month = str(today.year) + '/' + str(today.month) 
     for item in garbage_amount.keys():
         if year_month in total_amount[i]:
             total_amount[i][year_month] += garbage_amount[item]
         else:
             total_amount[i][year_month] = garbage_amount[item]
-        #reset
         garbage_amount[item] = 0
+    print(total_amount[0])
+    #response = jsonify({ 'pet' : total_amount[0], 'bin' : total_amount[1], 'can' : total_amount[2]})
+    response = jsonify(**total_amount[0])
+    response.status_code = 200
+    print(response)
+    return response
 
-    total_amount_pet = total_amount[0]
-    total_amount_bin = total_amount[1]
-    total_amount_can = total_amount[2]
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-    return render_template('reset.html',total_amount_pet=total_amount_pet, total_amount_bin=total_amount_bin,total_amount_can=total_amount_can)
-
-#def write_amount():
-=======
-
-    templateData = {
-        'time': timeString;
-        'num_pet':,
-        'num_bin':,
-        'num_cam':
-    }
-
-    return render_template('main.html', **templateData)
-
->>>>>>> 2b6664dd254c4eab8c67502ee99bc94de407a756
+@app.route('/location')
+def loc_map():
+    return render_template('/location.html')
 
 
 
 if __name__=="__main__":
-<<<<<<< HEAD
-    myThread = Triger()
-=======
->>>>>>> 2b6664dd254c4eab8c67502ee99bc94de407a756
-    app.run(host='0.0.0.0', port=80, debug=True)
+    #myThread = Triger()
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- static/reset.js
+++ static/reset.js
@@ -1,28 +1,55 @@
-var barChartData = {
-    labels : ["田中","山田","奥谷","ザビエル","おそ松","ピカチュウ","あひる"],
-    datasets : [
-        {
-          fillColor : /*"#d685b0"*/"rgba(214,133,176,0.7)",
-          strokeColor : /*"#d685b0"*/"rgba(214,133,176,0.7)",
-          highlightFill: /*"#eebdcb"*/"rgba(238,189,203,0.7)",
-          highlightStroke: /*"#eebdcb"*/"rgba(238,189,203,0.7)",
-          data : [20,45,1,20,19,33,96]
-        },
-        {
-          fillColor : /*"#7fc2ef"*/"rgba(127,194,239,0.7)",
-          strokeColor : /*"#7fc2ef"*/"rgba(127,194,239,0.7)",
-          highlightFill : /*"#a5d1f4"*/"rgba(165,209,244,0.7)",
-          highlightStroke : /*"#a5d1f4"*/"rgba(165,209,244,0.7)",
-          data : [2,54,77,32,9,78,95]
+//install react module
+//const React = require('react')
+//const ReactDOM = require('react-dom');
+
+
+
+window.onload = function drawGraph(){
+    $.ajax({
+        url : '/draw_graph',
+        type : 'GET',
+        dataType : 'json',
+        success : function(res){
+            alert( res.pet );
+/*
+            data_num = res.pet.length;
+            var label = new Array(data_num);
+            var pet_num = new Array(data_num);
+            var bin_num = new Array(data_num);
+            var can_num = new Array(data_num);
+            i = 0;
+            for(var item in res.pet){
+                pet_num[i] = res.pet[item];
+                i++;
+            }
+            j = 0;
+            for(var item in res.bin){
+                bin_num[j] = res.bin[item];
+                j++
+            }
+            k = 0;
+            for(var item in res.can.item){
+                can_num[k] = res.can[item];
+                k++;
+            }
+            $('div').text([res]);*/
         }
-      ]
-  
+    })
+} 
+
+/*
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['A','B','C','D','E','F','G'],
+        datasets: [{
+            label: 'apples',
+            data: [12, 19, 3, 17, 28, 24, 7]
+        }, {
+            label: 'oranges',
+            data: [30, 29, 5, 5, 20, 3, 10]
+        }]
     }
-    window.onload = function(){
-      var ctx = document.getElementById("chart").getContext("2d");
-      window.myBar = new Chart(ctx).Bar(barChartData, {
-        responsive : true,
-        // アニメーションを停止させる場合は下記を追加
-        /* animation : false */
-      });
-    }
+});
+*/
